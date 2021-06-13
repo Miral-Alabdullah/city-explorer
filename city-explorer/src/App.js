7341e8b938c2af342cc0289c13292f5c60bb3fe2
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-06-13 22:56:10 +0300

    Adding Bootstrap

--- city-explorer/src/App.js
+++ city-explorer/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
 export class App extends Component {
@@ -29,37 +31,56 @@ export class App extends Component {
 			toShowTheCity: true,
 			dataOfCities: axiosResponse.data[0]
 		})
-		console.log(axiosResponse);
 	}
 
 	render() {
+		const mystyle = {
+			color: "#fffbdf",
+			backgroundColor: '#564a4a',
+			padding: "10px",
+			fontFamily: "Arial"
+		  };
 		return (
-			<div>
-				<Form onSubmit={this.handleSubmitting}>
-					<Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-						<Form.Label column sm={2}>
-							City Name
-						</Form.Label>
-						<Col sm={10}>
-							<Form.Control type="text" placeholder="City Name" onChange={this.handleForm} />
-						</Col>
-					</Form.Group>
-					<Form.Group as={Row} className="mb-3">
-						<Col sm={{ span: 10, offset: 2 }}>
-							<Button type="submit" value="Get city">Explore!</Button>
-						</Col>
-					</Form.Group>
-				</Form>
-
-				{
-					this.state.toShowTheCity &&
-					<div>
-						<p>{this.state.dataOfCities.display_name}</p>
-						<img src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.dataOfCities.lat},${this.state.dataOfCities.lon}&zoom=15`} alt='' />
-					</div>
-				}
-
-			</div>
+			<>
+				<Container className="justify-content-md-center">
+					<Row className="text-center">
+						<div style={mystyle}>
+							<h1>City Explorer</h1>
+						</div>
+					</Row>
+					<Row className="justify-content-md-center">
+						<Col md="auto" ><div>
+							<Form onSubmit={this.handleSubmitting}>
+								<Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+									<Form.Label column sm={15} className="text-center" style={{fontSize: "30px"}}>
+										City Name
+										<Form.Control type="text" placeholder="City Name" onChange={this.handleForm} />
+									</Form.Label>
+								</Form.Group>
+								<Form.Group as={Row} className="mb-3">
+									<Col className="d-grid gap-2">
+										<Button type="submit" value="Get city" variant="outline-secondary" size="lg">Explore!</Button>
+									</Col>
+								</Form.Group>
+							</Form>
+						</div></Col>
+					</Row>
+					<Row className="justify-content-md-center">
+						<Col md="auto">{
+							this.state.toShowTheCity &&
+							<div>
+								<Card className="bg-dark text-white">
+									<Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.dataOfCities.lat},${this.state.dataOfCities.lon}&zoom=15`} alt="Card image" />
+									<Card.ImgOverlay>
+										<Card.Title style={{color: "#f55c47"}}>{this.state.cityName}</Card.Title>
+										<Card.Text style={{color: "#f55c47"}}>{this.state.dataOfCities.display_name}</Card.Text>
+									</Card.ImgOverlay>
+								</Card>
+							</div>
+						}</Col>
+					</Row>
+				</Container>
+			</>
 		)
 	}
 }
